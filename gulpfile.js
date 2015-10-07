ea479243748d7cf12cab6c2b1f73f9f4608aad62
nguyen manh <nguyenmanh1507@gmail.com>
2015-10-07 21:27:42 +0700

    add concat task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+// Concat task
+gulp.task('js', function() {
+	return gulp.src('src/**/*.js')
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('dist'))
+		;
+});
