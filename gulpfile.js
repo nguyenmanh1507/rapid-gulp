603c65ecb62e9a46fcb0fab7c90a86afadf065c7
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2015-10-12 14:51:16 +0700

    update gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,102 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
+'use strict';
 
-// Concat task
-gulp.task('js', function() {
-	return gulp.src('src/**/*.js')
-		.pipe(concat('all.js'))
-		.pipe(gulp.dest('dist'))
+var gulp              = require('gulp'),
+		sass              = require('gulp-sass'),
+		jshint            = require('gulp-jshint'),
+		rename            = require('gulp-rename'),
+		sourcemaps        = require('gulp-sourcemaps'),
+		wiredep           = require('wiredep').stream,
+		useref            = require('gulp-useref'),
+		gulpif            = require('gulp-if'),
+		uglify            = require('gulp-uglify'),
+		minifyCss         = require('gulp-minify-css'),
+		del               = require('del'),
+		browserSync       = require('browser-sync'),
+		autoprefixer      = require('gulp-autoprefixer')
+;
+
+// Scss task
+gulp.task('sass', function() {
+	return gulp.src('./app/scss/app.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./app/css'))
+		.pipe(browserSync.stream())
 		;
 });
 
-gulp.task('other-js', function() {
-	return gulp.src(['src/a.js', 'src/b.js'])
-		.pipe(concat('two.js'))
-		.pipe(gulp.dest('dist'))
+// Foundation CSS task
+gulp.task('sass-zf', function() {
+	return gulp.src('./app/bower_components/foundation/scss/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./app/css'))
 		;
 });
 
-gulp.task('default', ['js', 'other-js'], function() {
-	console.log('You dit it!')
+// JSHint task
+gulp.task('lint', function() {
+	return gulp.src('./app/js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'))
+		;
 });
+
+// Reload browser after lint task complete
+gulp.task('js-watch', ['lint'], browserSync.reload);
+
+// Bower task
+gulp.task('bower', function() {
+	return gulp.src('./app/*.html')
+		.pipe(wiredep({
+			exclude: ['./app/bower_components/modernizr/dist/modernizr-build.js']
+		}))
+		.pipe(gulp.dest('./app'))
+		;
+});
+
+// Useref task
+gulp.task('useref', ['del'], function() {
+	var assets = useref.assets();
+
+	return gulp.src('./app/*.html')
+		.pipe(assets)
+		.pipe(gulpif('*.js', uglify()))
+		.pipe(gulpif('*.css', minifyCss()))
+		.pipe(assets.restore())
+		.pipe(useref())
+		.pipe(gulp.dest('./dist'))
+		;
+});
+
+// Delete files task
+gulp.task('del', function() {
+	return del('./dist/**');
+});
+
+// BrowserSync task
+gulp.task('serve', function() {
+	browserSync.init({
+		server: './app'
+	});
+
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	gulp.watch('app/*.html').on('change', browserSync.reload);
+	gulp.watch('app/js/*.js', ['js-watch']);
+});
+
+// Autoprefixer CSS
+gulp.task('autoprefix', function() {
+	return gulp.src('./app/css/app.css')
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('./app/css'))
+		;
+});
+
+// Build task
+gulp.task('build', ['useref']);
+
+// Default task
+gulp.task('default', ['serve']);
